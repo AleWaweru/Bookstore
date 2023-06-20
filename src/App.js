94ee37416572fb241aaba951c128cf9eca3c9165
iamalex <ngashalex8@gmail.com>
2023-06-20 16:19:27 +0300

    Addbook button and Remove Button Functionality

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import Navbar from './components/Navbar';
 import Books from './components/pages/books/Books';
+import store from './redux/store';
 import Categories from './components/pages/Category/Categories';
 
 const App = () => (
   <div className="App">
-    <Navbar />
-    <Routes>
-      <Route path="/" element={<Books />} />
-      <Route path="/categories" element={<Categories />} />
-
-    </Routes>
+    <Provider store={store}>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Books />} />
+        <Route path="/categories" element={<Categories />} />
+      </Routes>
+    </Provider>
   </div>
 );
 
